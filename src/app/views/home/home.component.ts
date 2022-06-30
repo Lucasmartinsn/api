bf311a20cda575095b739d0b2468a0fba32f856e
Lucasmartinsn <106749361+Lucasmartinsn@users.noreply.github.com>
2022-06-30 16:40:19 -0300

    Add files via upload

---
+++ src/app/views/home/home.component.ts
@@ -0,0 +1,15 @@
+import { PessoasService } from './../../services/Pessoas.service';
+import { MatDialog } from '@angular/material/dialog';
+import { ElementDialogComponent } from './../../shared/element-dialog/element-dialog.component';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTable } from '@angular/material/table';
+import { Pessoa } from 'src/app/models/Pessoas';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent {
+  
+}
